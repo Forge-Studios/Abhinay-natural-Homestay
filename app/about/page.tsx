939d1998d2c206fb67717c9c46322d48aa85ac29
Abinash17a <https://github.com/Abinash17aChhetri>
2026-02-03 13:50:21 +0530

    width fixes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,6 @@ import Button from "@/components/base/Button";
 import Section from "@/components/base/Section";
 import ImageCarousel from "@/components/ImageCarousel";
 import StatCard from "@/components/StatCard";
-import TextCard from "@/components/TextCard";
 import resort from "@/public/images/resort.jpg";
 import Image from "next/image";
 
@@ -35,7 +34,7 @@ export default function AboutPage() {
   ];
 
   return (
-    <Section className="py-16">
+    <Section fullWidth={true} className="py-16">
       {/* HERO */}
       <section className="px-6 md:px-12 lg:px-20 py-16 md:py-28">
         <div className="max-w-[1400px] mx-auto grid grid-cols-1 lg:grid-cols-12 gap-12 items-center">
